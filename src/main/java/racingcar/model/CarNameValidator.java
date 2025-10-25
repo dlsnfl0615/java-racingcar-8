2dab3fcb7841ccbd41dc46b8deb57fa2d5300bc2
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-25 21:43:32 +0900

    feat($validator): add error case

---
+++ src/main/java/racingcar/model/CarNameValidator.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+public class CarNameValidator {
+    public static boolean validator(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자 이하여야 합니다.");
+        }
+        if (carName.equals(" ")) {
+            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+        }
+        return true;
+    }
+}
