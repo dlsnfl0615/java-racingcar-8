20e5c4c4e55eba7605c119b07def7f9366e9bb93
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-25 22:18:40 +0900

    test: unit test for validating lap format

---
+++ src/test/java/racingcar/model/LapValidatorTest.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LapValidatorTest {
+    @Test
+    @DisplayName("시도할 횟수로 올바른 형태")
+    void properNumberInput() {
+        LapValidator validator = new LapValidator();
+        Assertions.assertThat(validator.validate("12")).isTrue();
+    }
+
+    @Test
+    @DisplayName("시도할 횟수로 문자 입력")
+    void inputWithCharacter() {
+        LapValidator validator = new LapValidator();
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () ->
+            validator.validate("1a2c3")
+        );
+    }
+}
