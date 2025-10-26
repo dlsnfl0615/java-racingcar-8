9c2ea88d2bcd76c63c856a23cc91aa80ba657e8f
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 16:58:44 +0900

    feat(winner): modify the winner to the furthest car

--- src/main/java/racingcar/model/GetWinner.java
+++ src/main/java/racingcar/model/GetWinner.java
@@ -1,32 +1,18 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GetWinner {
-    public List<Car> winner(List<Car> cars, int lap) {
-        List<Car> winner = new ArrayList<>();
-
+    public List<Car> winner(List<Car> cars) {
+        Car maxDistanceCar = Collections.max(cars);
+        List<Car> winnerCars = new ArrayList<>();
         for (Car car : cars) {
-            if (car.isFinished(lap)) {
-                winner.add(car);
+            if (car.equals(maxDistanceCar)) {
+                winnerCars.add(car);
             }
         }
-        if (!winner.isEmpty()) {
-            return winner;
-        }
-
-        int maxDistance = 0;
-        for (Car car : cars) {
-            if (car.getMoveDistance() > maxDistance) {
-                maxDistance = car.getMoveDistance();
-            }
-        }
-        for (Car car : cars) {
-            if (car.getMoveDistance() == maxDistance) {
-                winner.add(car);
-            }
-        }
-        return winner;
+        return winnerCars;
     }
 }
