8ba8b451687814661635be0da9ac34b843188c14
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 21:30:44 +0900

    feat($RacingController): manage events

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,27 @@
+package racingcar.controller;
+
+import racingcar.model.RaceManager;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RaceManager raceManager;
+
+    public RacingController(InputView inputView, OutputView outputView, RaceManager raceManager) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.raceManager = raceManager;
+    }
+
+    public void run() {
+        String carNames = inputView.inputNameLap();
+        String lap = inputView.inputNameLap();
+
+        raceManager.initNameLap(carNames, lap);
+        String winners = raceManager.manage();
+
+        outputView.printResult(winners);
+    }
+}
