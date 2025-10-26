0290d29fc3422bc9c92c54aa5baa4a147ae5345f
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 21:28:46 +0900

    feat($RaceManager): manage the whole race flow

---
+++ src/main/java/racingcar/model/RaceManager.java
@@ -0,0 +1,45 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceManager {
+    private int lap;
+    private List<Car> cars = new ArrayList<>();
+    private final MoveForward moveForward = new MoveForward();
+    private final GetWinner getWinner = new GetWinner();
+    private final CarNameParser carNameParser = new CarNameParser();
+    private final LapValidator lapValidator = new LapValidator();
+
+    public void initNameLap(String carNames, String lap) {
+        List<String> seperatedNames = carNameParser.parse(carNames);
+        for (int i = 0; i < seperatedNames.size(); i++) {
+            cars.add(new Car(seperatedNames.get(i), 0));
+        }
+
+        this.lap = lapValidator.validate(lap);
+    }
+
+    public String manage() {
+        for (int i = 0; i < lap; i++) {
+            moveCar();
+            printDistancePerLap();
+        }
+
+        List<Car> winners = getWinner.winner(cars);
+        return getWinner.winnerListToString(winners);
+    }
+
+    public void moveCar() {
+        for (int i = 0; i < cars.size(); i++) {
+            moveForward.move(cars.get(i));
+        }
+    }
+
+    public void printDistancePerLap() {
+        for (Car car : cars) {
+            car.getDistance();
+        }
+        System.out.println("==================");
+    }
+}
