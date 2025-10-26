c355207c24f10d74822f0587b6f43023539807be
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 21:14:42 +0900

    feat($validate): make method return integer

--- src/main/java/racingcar/model/LapValidator.java
+++ src/main/java/racingcar/model/LapValidator.java
@@ -1,12 +1,14 @@
 package racingcar.model;
 
 public class LapValidator {
-    public boolean validate(String lap) {
+    public int validate(String lap) {
+        int returnLap;
         try {
-            Integer.parseInt(lap);
+            returnLap = Integer.parseInt(lap);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도할 횟수는 숫자 형태여야 합니다.");
         }
-        return true;
+
+        return returnLap;
     }
 }
