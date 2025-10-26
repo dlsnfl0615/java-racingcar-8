4a8b6c203bcf1ef7ea9bb4b2a57c92d5e59fd537
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 21:16:02 +0900

    style($printResult): "winner" to "winners"

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public class OutputView {
-    public void printResult(String winner) {
-        System.out.println(winner);
+    public void printResult(String winners) {
+        System.out.println(winners);
     }
 }
