11c60df93c3b004cda9cba76a062ac886fbf38d4
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 23:59:02 +0900

    feat($printRaceProgress): extract print feature from RaceManager class

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,16 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     public void printResult(String winners) {
         System.out.println(winners);
+    }
+
+    public void printRaceProgress(List<String> distanceStrings) {
+        for (String distance : distanceStrings) {
+            System.out.println(distance);
+        }
+        System.out.println("==================");
     }
 }
