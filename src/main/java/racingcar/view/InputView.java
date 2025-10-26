6ea28520cfcc31b3a140c28da40362da9ab9d526
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-26 17:21:01 +0900

    feat($inputCarNames,$inputLap): read strings from user

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputCarNames() {
+        return Console.readLine();
+    }
+    public String inputLap() {
+        return Console.readLine();
+    }
+}
