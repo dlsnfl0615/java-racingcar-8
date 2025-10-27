f2d54e78c5dfb24da019256a56897ae274242569
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 15:59:45 +0900

    refactor($winnerListToString): delete useless method

--- src/main/java/racingcar/model/GetWinner.java
+++ src/main/java/racingcar/model/GetWinner.java
@@ -16,13 +16,4 @@ public class GetWinner {
         }
         return winnerCars;
     }
-
-    public String winnerListToString(List<Car> winners) {
-        String result = "최종 우승자 : ";
-        for (Car car : winners) {
-            result += car.getWinner() + ", ";
-        }
-        result = result.substring(0, result.length() - 2);
-        return result;
-    }
 }
