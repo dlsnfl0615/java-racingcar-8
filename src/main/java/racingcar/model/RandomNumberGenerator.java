9381b956fead2a814822ac0a1d49b7e910d34ef7
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 12:22:06 +0900

    refactor($move): change class name (GetRandomNumber to RandomNumberGenerator)

--- src/main/java/racingcar/model/RandomNumberGenerator.java
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -2,7 +2,7 @@ package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class GetRandomNumber {
+public class RandomNumberGenerator {
     public static boolean random() {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
