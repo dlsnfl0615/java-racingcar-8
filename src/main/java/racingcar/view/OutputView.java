a0b5919c47f0e6484f3192f6f24e1a956d935464
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 15:05:06 +0900

    feat($formatWinners,$printWinners): add print feature from model layer

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,28 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
 import java.util.List;
 
 public class OutputView {
-    public void printResult(String winners) {
-        System.out.println(winners);
-    }
-
     public void printRaceProgress(List<String> distanceStrings) {
         for (String distance : distanceStrings) {
             System.out.println(distance);
         }
         System.out.println("==================");
+    }
+
+    public String formatWinners(List<Car> winners) {
+        String result = "최종 우승자 : ";
+        for (Car car : winners) {
+            result += car.getWinner() + ", ";
+        }
+        result = result.substring(0, result.length() - 2);
+
+        return result;
+    }
+
+    public void printWinners(List<Car> winners) {
+        System.out.println(formatWinners(winners));
     }
 }
