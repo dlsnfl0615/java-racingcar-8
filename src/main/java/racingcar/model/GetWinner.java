bf7ebe10b087ce2b28d005ebfbc9ac3c649c2bd7
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 15:03:15 +0900

    style: add comment

--- src/main/java/racingcar/model/GetWinner.java
+++ src/main/java/racingcar/model/GetWinner.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class GetWinner {
+    // 가장 멀리 이동한 자동차의 이동거리가 우승자의 기준이 됨
     public List<Car> winner(List<Car> cars) {
         Car maxDistanceCar = Collections.max(cars);
         List<Car> winnerCars = new ArrayList<>();
