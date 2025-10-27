a4ffb78fa0f4d485b6dc840c5554ac5917d29de2
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 15:56:40 +0900

    refactor($getDistance,$getName): move output formatting logic from model to view layer

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -30,18 +30,9 @@ public class Car implements Comparable<Car> {
         return this.moveDistance == ((Car) obj).moveDistance;
     }
 
-    // 이동 칸 수(숫자)를 문자열로 시각화하기
-    public String getDistance() {
-        String result = "";
-        result = name + " : ";
-        for (int i = 0; i < moveDistance; i++) {
-            result += "-";
-        }
+    public int getDistance() { return moveDistance; }
 
-        return result;
-    }
-
-    public String getWinner() {
+    public String getName() {
         return name;
     }
 }
