9b2f7ae1b3a5b98217f5af7e6cf3208e9694eab6
dlsnfl0615 <hyunseo0750@gmail.com>
2025-10-27 19:34:35 +0900

    feat($parse): validate duplicate names

--- src/main/java/racingcar/model/CarNameParser.java
+++ src/main/java/racingcar/model/CarNameParser.java
@@ -1,15 +1,23 @@
 package racingcar.model;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CarNameParser {
     public List<String> parse(String input) {
         String[] names = input.split(",");
+        Set<String> uniqueNames = new HashSet<>();
 
         return Arrays.stream(names)
                 .peek(CarNameValidator::isValid) // 이름 입력 양식 검증
+                .peek(name -> {
+                    if (!uniqueNames.add(name)) {
+                        throw new IllegalArgumentException("자동차 이름이 중복됩니다.");
+                    }
+                })
                 .collect(Collectors.toList());
     }
 }
